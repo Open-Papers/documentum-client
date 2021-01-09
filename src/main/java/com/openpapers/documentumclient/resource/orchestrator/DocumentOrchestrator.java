50173d258557e2778850aaf8f652f9d43f61af5a
Utsab Chowdhury <utsab.c97@gmail.com>
2021-01-09 12:47:09 +0530

    On-boarding content Service, to store document content

--- src/main/java/com/openpapers/documentumclient/resource/orchestrator/DocumentOrchestrator.java
+++ src/main/java/com/openpapers/documentumclient/resource/orchestrator/DocumentOrchestrator.java
@@ -3,13 +3,24 @@ package com.openpapers.documentumclient.resource.orchestrator;
 
 import com.openpapers.documentumclient.resource.model.GreenDocument;
 import com.openpapers.documentumclient.resource.search.SearchParameter;
+import com.openpapers.documentumclient.resource.service.ContentService;
 import com.openpapers.documentumclient.resource.service.ICoreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Collection;
+import java.util.Map;
 
 
 public class DocumentOrchestrator {
 
+    private final Logger logger = LoggerFactory.getLogger(DocumentOrchestrator.class);
+
+    @Autowired
+    ContentService contentService;
 
     private ICoreService service;
 
@@ -30,7 +41,13 @@ public class DocumentOrchestrator {
 
     }
 
-    public GreenDocument save(GreenDocument doc) {
+    public GreenDocument save(GreenDocument doc, MultipartFile file) {
+        try{
+            this.handleContent(doc,file);
+        }catch (Exception ex){
+            logger.error("Error at save, handle content",ex);
+        }
+
         return service.save(doc);
     }
 
@@ -44,5 +61,14 @@ public class DocumentOrchestrator {
 
     public Collection<GreenDocument> search(SearchParameter searchparam) {
         return service.search(searchparam);
+    }
+
+    public GreenDocument handleContent(GreenDocument doc, MultipartFile file)throws IOException {
+
+        Map map = contentService.initiateContentUpload(file);
+        doc.setContentSize(String.valueOf(file.getBytes().length/1024*1024));
+        doc.setContentLink((String)map.getOrDefault("file_url",""));
+        doc.setContentKey((String)map.getOrDefault("file_key",""));
+        return doc;
     }
 }
